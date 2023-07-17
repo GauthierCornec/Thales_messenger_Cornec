c4a7eaef8d4c0e4421508aed0c0e4dbc7691278b
GauthierCornec <gauthier.cornec@gmail.com>
2023-07-17 11:57:04 +0200

    add responsive

--- src/pages/Messenger/messenger.js
+++ src/pages/Messenger/messenger.js
@@ -12,6 +12,7 @@ const Messenger = () => {
     const [selectedContactInfo, setSelectedContactInfo] = useState(null);
     const [selectedConversationId, setSelectedConversationId] = useState(null); // Added state for selected conversation ID
     const [selectedContactMessages, setSelectedContactMessages] = useState([]); // Added state for selected contact's messages
+    const [isConversationSelected, setIsConversationSelected] = useState(false); // Added state for tracking if a conversation is selected
   
     useEffect(() => {
       AsyncStorage.getItem('token').then((value) => {
@@ -63,43 +64,58 @@ const Messenger = () => {
         };
   
         axios.post(`http://${process.env.REACT_APP_API_URL}/messages`, payload, {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + value,
-          }
-        })
-          .then((response) => {
-            // Traitement des données de réponse si nécessaire
-            console.log('Message envoyé avec succès');
-            // Réinitialiser le champ de saisie de texte
-            setMessageContent('');
-          })
-          .catch((error) => {
-            // Gérer les erreurs de requête si nécessaire
-            console.error('Erreur lors de l envoi du message', error);
-          });
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + value,
+  }
+})
+  .then((response) => {
+    // Traitement des données de réponse si nécessaire
+    console.log('Message envoyé avec succès');
+
+    // Ajouter le nouveau message à la liste des messages existants
+    const newMessage = {
+      id: response.data.id,
+      senderId: userData.id,
+      data: messageContent,
+      createdAt: Date.now()
+    };
+    setSelectedContactMessages((prevMessages) => [...prevMessages, newMessage]);
+
+    // Réinitialiser le champ de saisie de texte
+    setMessageContent('');
+  })
+  .catch((error) => {
+    // Gérer les erreurs de requête si nécessaire
+    console.error('Erreur lors de l envoi du message', error);
+  });
+
       });
     };
   
     const setSelectedContact = (contact) => {
-      setSelectedContactInfo(contact);
-      const selectedConversation = userData.conversations.find(conversation => conversation.contactId === contact.id);
-      if (selectedConversation) {
-        setSelectedConversationId(selectedConversation.id);
-        setSelectedContactMessages(selectedConversation.messages);
-      } else {
-        setSelectedConversationId(null);
-        setSelectedContactMessages([]);
-      }
+        setSelectedContactInfo(contact);
+        const selectedConversation = userData.conversations.find(conversation => conversation.contactId === contact.id);
+        if (selectedConversation) {
+            setSelectedConversationId(selectedConversation.id);
+            setSelectedContactMessages(selectedConversation.messages);
+            setIsConversationSelected(true); // Set isConversationSelected to true when a conversation is selected
+        } else {
+            setSelectedConversationId(null);
+            setSelectedContactMessages([]);
+            setIsConversationSelected(false); // Set isConversationSelected to false when no conversation is selected
+        }
     };
-      
+    
+
     return (
+        
         <div className='flex'>
-            <div className="flex flex-col bg-slate-100 max-w-sm h-screen px-2">
-                <p className='font-sans text-2xl my-2'>Discutions</p>
-                <CreateConversationButton updateConversations={updateConversations}/>
-                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                <div className="relative mb-4">
+            <div className={`flex flex-col bg-slate-100 max-w-sm ${isConversationSelected ? 'hidden lg:block' : 'block'} h-screen px-2`}>
+          <p className='font-sans text-2xl my-2'>Discussions</p>
+          <CreateConversationButton updateConversations={updateConversations} />
+          <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+          <div className="relative mb-4">
                     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                         <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
@@ -155,52 +171,62 @@ const Messenger = () => {
 
             </div>
 
-            <div className="flex flex-col w-3/4 relative mx-4 bg-slate-100">
-                    <div className='flex h-20 border-4 border-black'>
-                        {selectedContactInfo && (
-                        <><img className='h-full w-auto' src={profile} alt="Profile" />
-                        <div className='flex flex-row justify-between'>
-                            <p>{selectedContactInfo.firstName}</p>
-                            <p>{selectedContactInfo.lastName}</p>
-                            {/* Autres informations du contact */}
-                        </div></>
-                        )}
+            <div className={`flex flex-col w-full relative mx-4 bg-slate-100 max-h-screen ${selectedContactInfo ? 'block' : 'hidden'}`}>
+        <div className="h-20 flex my-8 mx-4 place-items-center">
 
-                    </div>
-                    {selectedContactInfo && selectedContactMessages.length > 0 && (
-          <div className="flex-grow overflow-hidden border-4 border-yellow-500 h-fit">
-            <div className="flex flex-col-reverse">
-              {selectedContactMessages.map((message) => (
-                <div
-                  key={message.id}
-                  className={`self-${message.senderId === userData.id ? 'end' : 'start'} bg-${message.senderId === userData.id ? 'blue-500' : 'gray-200'} text-white p-2 rounded-lg m-2`}
-                >
-                  {message.data}
+                    {selectedContactInfo && (
+                    <>
+                        <img className="h-full w-auto" src={profile} alt="Profile" />
+                        <div className="flex flex-row space-x-1 ml-2">
+                        <p className='mx-auto'>{selectedContactInfo.firstName}</p>
+                        <p>{selectedContactInfo.lastName}</p>
+                        {/* Autres informations du contact */}
+                        </div>
+                    </>
+                    )}
                 </div>
-              ))}
-            </div>
-          </div>
-        )}
+                <div className=" mb-12 overflow-auto h-screen">
+                    <div className='overflow-y'>
+                    {selectedContactMessages.map((message) => {
+                    const createdAt = new Date(message.createdAt);
+                    const formattedTime = createdAt.toLocaleTimeString([], {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                    });
+                    return (
+                        <div
+                        key={message.id}
+                        className={`flex justify-end p-2 m-2 rounded-lg ${
+                            message.senderId === userData.id ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'
+                        }`}
+                        >
+                        <div className="flex flex-row justify-items-center space-x-2">
+                            <p className='mb-1'>{message.data}</p>
+                            <p className="text-xs text-gray-500">{formattedTime}</p>
+                        </div>
+                        </div>
+                    );
+                    })}
+                    </div>
                     
+                </div>
 
-                                    
-                    <div className="absolute bottom-0 left-0 flex flex-row w-full space-between border-4 border-green-500">
+                <div className="absolute bottom-0 left-0 flex flex-row w-full space-between">
                     <input
-                        type="text"
-                        placeholder="Entrez votre message"
-                        className="w-full h-full"
-                        value={messageContent}
-                        onChange={(e) => setMessageContent(e.target.value)}
+                    type="text"
+                    placeholder="Entrez votre message"
+                    className="w-full h-full"
+                    value={messageContent}
+                    onChange={(e) => setMessageContent(e.target.value)}
                     />
                     <div>
                     <button className="px-4 py-2 bg-blue-500 text-white" onClick={sendMessage}>
                         Envoyer
                     </button>
+                    
                     </div>
                 </div>
             </div>
-            
-
         </div>
     )
 }
